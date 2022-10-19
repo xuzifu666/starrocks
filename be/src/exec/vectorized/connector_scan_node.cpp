d03d9fbcd2110acd5578ce23e48533b10dc37d8b
xuyu <xuyu@zepp.com>
2022-10-19 10:12:28 +0800

    pipeline improment

--- be/src/exec/vectorized/connector_scan_node.cpp
+++ be/src/exec/vectorized/connector_scan_node.cpp
@@ -172,6 +172,9 @@ Status ConnectorScanNode::_start_scan_thread(RuntimeState* state) {
     // init chunk pool
     _pending_scanners.reverse();
     _num_scanners = _pending_scanners.size();
+    if (_num_scanners == 0) {
+        return Status::EndOfFile("");
+    }
     _chunks_per_scanner = config::scanner_row_num / state->chunk_size();
     _chunks_per_scanner += static_cast<int>(config::scanner_row_num % state->chunk_size() != 0);
     int concurrency = std::min<int>(config::max_hdfs_scanner_num, _num_scanners);
@@ -207,6 +210,10 @@ Status ConnectorScanNode::get_next(RuntimeState* state, ChunkPtr* chunk, bool* e
     SCOPED_TIMER(_runtime_profile->total_time_counter());
     if (!_start && _status.ok()) {
         Status status = _start_scan_thread(state);
+        if (status.is_end_of_file()) {
+            *eos = true;
+            return Status::OK();
+        }
         _update_status(status);
         LOG_IF(ERROR, !status.ok()) << "Failed to start scan node: " << status.to_string();
         _start = true;
